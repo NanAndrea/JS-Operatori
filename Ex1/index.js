eaf5b4426073dc0b3bdb193d750ab38cb00f33fb
Nan Andrea <balajelandrea@yahoo.com>
2023-01-12 15:21:48 +0200

    changed var to const

--- Ex1/index.js
+++ Ex1/index.js
@@ -1,15 +1,15 @@
-var a = 2 + 3 * 4 - 5;
+const a = 2 + 3 * 4 - 5;
 console.log("Rezultatul este: ", a);
 
-var b = 4 * (2 + 6 / 3);
+const b = 4 * (2 + 6 / 3);
 console.log("Rezultatul este: ", b);
 
-var c = 2 * 3 ** 2 + 2 ** 5;
+const c = 2 * 3 ** 2 + 2 ** 5;
 console.log("Rezultatul este: ", c);
 
-var d = (5 * (3 + 2 / 2)) + 3;
+const d = (5 * (3 + 2 / 2)) + 3;
 console.log("Rezultatul este: ", d);
 
-var e = (2 ** 5 - 5 ** 2 + 3 * (5 - 4 / 2)) / 2 ** 5 + 5 ** 2;
+const e = (2 ** 5 - 5 ** 2 + 3 * (5 - 4 / 2)) / 2 ** 5 + 5 ** 2;
 console.log("Rezultatul este: ", e);
 
